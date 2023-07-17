df4c1dadce28e0c90bf63165e20ecd691a6e0173
Agus Sugandi <agus.sugandi95@gmail.com>
2023-07-17 16:54:38 +0700

    first commit

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,51 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth from "next-auth";
+import type { NextAuthOptions } from "next-auth";
+import { User } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    CredentialsProvider({
+      type: "credentials",
+      credentials: {},
+      authorize(credentials, req) {
+        const { username, password } = credentials as {
+          username: string;
+          password: string;
+        };
+        // perform you login logic
+        // find out user from db
+        if (username !== "john@gmail.com" || password !== "1234") {
+          throw new Error("invalid credentials");
+        }
+
+        // if everything is fine
+        return {
+          id: "1234",
+          name: "John Doe",
+          username: "john@gmail.com",
+          role: "admin",
+        };
+      },
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.user = token;
+
+      return session;
+    },
+  },
+
+  pages: {
+    signIn: "/auth/login",
+  },
+};
+
+export default NextAuth(authOptions);
